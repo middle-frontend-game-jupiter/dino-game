f8abf9bdf278671eff81b18b6232d0f705ab699f
Nikita Lavrentev <n.lawrentiew2017@yandex.ru>
2023-02-28 16:05:37 +0600

    added reset.css

---
+++ packages/client/src/app/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { withHocs } from './hocs'
+import './styles/index.css'
+import { routeConfig } from '@/shared/config/routeConfig/routeConfig'
+import Grid from '@mui/material/Grid'
+
+const App = () => {
+
+  return (
+    <Routes>
+      {Object.values(routeConfig).map(({ path, element }) => (
+        <Route
+          key={path}
+          path={path}
+          element={(
+            <Grid>
+              {element}
+            </Grid>
+          )}
+        />
+      ))}
+    </Routes>
+  )
+}
+
+export default withHocs(App)
+
