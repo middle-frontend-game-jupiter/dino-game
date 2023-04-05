e4724b516eeab3d5dae3483262f6662de88db4b0
Nikita Lavrentev <n.lawrentiew2017@yandex.ru>
2023-04-05 18:58:57 +0400

    fix layout

--- packages/client/src/pages/Settings/ui/FormFields.tsx
+++ packages/client/src/pages/Settings/ui/FormFields.tsx
@@ -1,30 +1,26 @@
 import React, { memo } from 'react'
-
 import { TextFieldForm } from '@/shared/hocs/formHocs'
 import { UserEntity } from '@/shared/types/User'
 import { Box } from '@mui/material'
 import { Field } from 'react-final-form'
 import { Labels } from '../lib/config'
 
-
-export const FormFields = memo<unknown | any>(({ user }: { user: UserEntity }) => 
-  Object
-    .keys(user)
-    .map((key, index) => (
+export const FormFields = memo<unknown | any>(
+  ({ user }: { user: UserEntity }) =>
+    Object.keys(user).map((key, index) => (
       <Box
         display="flex"
         key={`${key}-${index}`}
         justifyContent="center"
         alignItems="center"
         marginBottom="10px"
-        width="500px"
-      >            
-        <Field 
+        width="500px">
+        <Field
           sx={{ width: '100%' }}
           name={key}
           label={Labels[key]}
           component={TextFieldForm}
-        /> 
-     </Box>
-    )
-))
+        />
+      </Box>
+    ))
+)
--- packages/client/src/pages/Settings/ui/UserSettingsForm.tsx
+++ packages/client/src/pages/Settings/ui/UserSettingsForm.tsx
@@ -1,60 +1,53 @@
-import React, { FC } from "react"
-
-import { UserEntity } from "@/shared/types/User"
-import { Alert, AlertTitle, Grid, Box } from "@mui/material"
-import { Form } from "react-final-form"
-import { LoadingButton } from "@mui/lab"
-
-import { FormFields } from "./FormFields"
-
-import validate from "../lib/validate"
+import React, { FC } from 'react'
+import { UserEntity } from '@/shared/types/User'
+import { Alert, AlertTitle, Grid, Box } from '@mui/material'
+import { Form } from 'react-final-form'
+import { LoadingButton } from '@mui/lab'
+import { FormFields } from './FormFields'
+import validate from '../lib/validate'
 
 type UserSettingsFormProps = {
-  user: Omit<UserEntity, 'avatar' | 'id'>;
-  onChangeUser: (user: UserEntity) => void;
-  isError?: boolean;
-  isLoading: boolean;
-  reason?: string | null;
+  user: Omit<UserEntity, 'avatar' | 'id'>
+  onChangeUser: (user: UserEntity) => void
+  isError?: boolean
+  isLoading: boolean
+  reason?: string | null
 }
 
-export const UserSettingsForm: FC<UserSettingsFormProps> = ({ 
-  user, 
-  onChangeUser, 
+export const UserSettingsForm: FC<UserSettingsFormProps> = ({
+  user,
+  onChangeUser,
   isLoading,
   isError,
-  reason
+  reason,
 }: UserSettingsFormProps) => (
-  <Form 
-    initialValues={user} 
+  <Form
+    initialValues={user}
     onSubmit={onChangeUser}
     validate={validate}
     initialValuesEqual={() => true}
     render={({ handleSubmit }) => (
       <form onSubmit={handleSubmit}>
         <Grid width="500px">
-          {
-            isError && (
-              <Box marginBottom='10px'>
-                <Alert severity="error">
-                  <AlertTitle>{reason}</AlertTitle>
-                </Alert>
-              </Box>
-            )
-          }
+          {isError && (
+            <Box marginBottom="10px">
+              <Alert severity="error">
+                <AlertTitle>{reason}</AlertTitle>
+              </Alert>
+            </Box>
+          )}
           <FormFields user={user} />
 
-          <LoadingButton 
+          <LoadingButton
             type="submit"
             variant="contained"
             disabled={isLoading}
             fullWidth
-            loading={isLoading}
-          >
+            loading={isLoading}>
             Change User
           </LoadingButton>
         </Grid>
       </form>
-      )
-    }
+    )}
   />
 )
